3fcfbb03f04a3cb4f63326d724263facdd5cd6da
MARÍA DE LAS NIEVES DOMÍNGUEZ NAVARRO <mnieves.domnav@educa.jcyl.es>
2025-11-21 10:23:44 +0100

    Solucionando bugs de slash commands.

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ iniciar_servidor()
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user}')
-    # Sincroniza globalmente todos los slash commands
     synced = await bot.tree.sync()
     print(f"Slash commands globales sincronizados: {len(synced)} comandos.")
 
 async def main():
-    async with bot:
-        # Carga tus cogs normalmente, no hace falta registrar comando por comando
-        await bot.load_extension("commands.generales")
-        await bot.load_extension("commands.cartas")
-        await bot.load_extension("commands.wiki")
-        await bot.load_extension("commands.moderation")
-        await bot.load_extension("commands.auto_cards")
-        await bot.load_extension("commands.battle")
-        await bot.start(TOKEN)
+    # Carga cogs normalmente
+    await bot.load_extension("commands.generales")
+    await bot.load_extension("commands.cartas")
+    await bot.load_extension("commands.wiki")
+    await bot.load_extension("commands.moderation")
+    await bot.load_extension("commands.auto_cards")
+    await bot.load_extension("commands.battle")
+
+    # Inicia el bot SIN usar 'async with bot'
+    await bot.start(TOKEN)
 
 asyncio.run(main())
