3df758fcd9b494942e57b79e6063ec145a63eedf
nievesdom <nievesdomnav@gmail.com>
2025-11-22 14:07:42 +0100

    Los comandos se sicronizan en todos los servidores y en el de pruebas automáticamente

--- commands/generales.py
+++ commands/generales.py
@@ -52,7 +52,7 @@ class Generales(commands.Cog):
         # Ejecuta la función de conteo como tarea asincrónica
         asyncio.create_task(contar_mensaje())
 
-    @app_commands.command(name="updates", description="Show the latest updates and what's coming up.")
+    @app_commands.command(name="updates", description="Shows the latest updates and what's coming up.")
     async def updates(self, interaction: discord.Interaction):
         await interaction.response.send_message(
             "**Version:** 1.1\n**Patch notes:**\n- The bot is now compatible with slash commands. You can use the commands with `/` as a prefix instead of `y!` and discord will tell you when and how to introduce arguments to a command, making it easier to use commands such as `/trade`.\n- Fixed a bug that would cause the bot to quickly reach the request limit while trying to save the automatic cards information from multiple servers at the same time. I never expected this bot to be in more than a couple of servers.\n"
